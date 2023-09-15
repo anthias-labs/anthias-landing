4510c26e224b0565c81acd187edfebc35f936542
alexcraig <alexcraig043@gmail.com>
2023-09-14 20:00:27 -0400

    first commit

---
+++ pages/liquip/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <embed
+        src="/liquip.pdf"
+        type="application/pdf"
+        style={{ height: "100%", width: "100%" }}
+      />
+    </div>
+  );
+}
